ede1faef1e0192aad50a481bfaed7504b7f9cc39
gvarys <johnnyzaev@gmail.com>
2023-01-23 12:40:53 +0600

    PlayerSprite is now flipping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,15 +7,18 @@ public class Player : MonoBehaviour
 	private float _horizontalMovement;
 	private Vector2 _playerVelocity;
 	private Rigidbody2D _playerRb;
+	private Vector3 _playerScale;
 
 	private void Start()
 	{
+		_playerScale = transform.localScale;
 		_playerRb = GetComponent<Rigidbody2D>();
 	}
 
 	private void Update()
 	{
 		Run();
+		FlipSprite();
 	}
 
 	private void Run()
@@ -24,5 +27,15 @@ public class Player : MonoBehaviour
 		_playerVelocity.x = _horizontalMovement * playerSpeed;
 		_playerVelocity.y = _playerRb.velocity.y;
 		_playerRb.velocity = _playerVelocity;
+	}
+
+	private void FlipSprite()
+	{
+		bool playerHasHorizontalSpeed = Mathf.Abs(_playerRb.velocity.x) > Mathf.Epsilon;
+		if (playerHasHorizontalSpeed)
+		{
+			_playerScale.x = Mathf.Sign(_playerVelocity.x);
+			transform.localScale = _playerScale;
+		}
 	}
 }
