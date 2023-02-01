a089ae15bce5b45a6d70e9477199c067f5ec962a
gvarys <johnnyzaev@gmail.com>
2023-02-01 15:22:27 +0600

    now exit adds a little slow motion to the game

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,6 +6,14 @@ public class Exit : MonoBehaviour
 {
 	private void OnTriggerEnter2D(Collider2D col)
 	{
+		StartCoroutine(LoadNextLevel());
+	}
+
+	private IEnumerator LoadNextLevel()
+	{
+		Time.timeScale = 0.2f;
+		yield return new WaitForSecondsRealtime(2f);
+		Time.timeScale = 1f;
 		var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
 		SceneManager.LoadScene(currentSceneIndex + 1);
 	}
