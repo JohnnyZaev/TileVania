cf34f46b018cc1ad0eb192d97dd9a522a8eefdb7
gvarys <johnnyzaev@gmail.com>
2023-02-10 15:45:40 +0600

    Coins now have sounds

--- Assets/Scripts/CoinPickups.cs
+++ Assets/Scripts/CoinPickups.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class CoinPickups : MonoBehaviour
 {
+	[SerializeField] private AudioClip coinSound;
+	
 	private void OnTriggerEnter2D(Collider2D col)
 	{
+		if (Camera.main != null) AudioSource.PlayClipAtPoint(coinSound, Camera.main.transform.position);
 		Destroy(gameObject);
 	}
 }
